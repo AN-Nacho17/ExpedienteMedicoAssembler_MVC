0a86d73426bf6ee5016fb63d2d79869ffbee6973
STEVEN JESUS RAMIREZ CERDAS <steven.ramirezcerdas@ucr.ac.cr>
2022-07-02 13:34:59 -0600

    Addin identity

--- ExpedienteMedico/Models/User.cs
+++ ExpedienteMedico/Models/User.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace ExpedienteMedico.Models
 {
-    public class User
+    public class User : IdentityUser
     {
 
         [Key]
--- ExpedienteMedico/Program.cs
+++ ExpedienteMedico/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ExpedienteMedico.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ExpedienteMedicoContextConnection") ?? throw new InvalidOperationException("Connection string 'ExpedienteMedicoContextConnection' not found.");
+
+builder.Services.AddDbContext<ExpedienteMedicoContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ExpedienteMedicoContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
